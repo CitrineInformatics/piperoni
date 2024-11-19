f7178ed605359088ef9699cad4ee0e1b9c68ae41
Ventura Rivera <35743526+ventura-rivera@users.noreply.github.com>
2024-11-19 07:42:03 -0800

    Python 3.10 -> 3.9

--- setup.py
+++ setup.py
@@ -31,6 +31,6 @@ setup(
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.9',
     ],
 )
